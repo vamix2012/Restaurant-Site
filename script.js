22549ad4cbe3de7552727012bb3eccc973497a47
Lucian-Cristian Toma <vamix2012@gmail.com>
2020-11-28 01:48:19 +0100

    My very first project with responsive design

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+
+    document.getElementById('menu').classList.add('show-overlay-menu');
+}
+
+function hideMenu() {
+    document.getElementById('menu').classList.remove('show-overlay-menu');
+}
